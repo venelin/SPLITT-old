104ccf62b8cd34cb71bace7d77216ce4c7582772
Venelin Mitov <venelin.mitov@bsse.ethz.ch>
2017-11-15 18:16:53 +0100

    adding ThreePointMultivariate, which includes <armadillo>

---
+++ src/ThreePointMultivariate.h
@@ -0,0 +1,111 @@
+//   Copyright 2017 Venelin Mitov
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+//   limitations under the License.
+
+#ifndef ParallelPruning_ThreePointMultivariate_H_
+#define ParallelPruning_ThreePointMultivariate_H_
+
+#include "ParallelPruning.h"
+#include <armadillo>
+
+// Calculate the |V| and quadratic quantities of the form Q=X'V^(-1)Y, for
+// any covariance matrix V within the class of “3-point structured” matrices.
+// X and Y must have the same number of rows as V but can have any number of
+// columns. The algorithm also needs to calculate the following quantities
+// p=1′V^(−1)1,  hat{mu}_Y=1′V^(−1)Y/p, and tilde{mu}_X'=X′V^(−1)1/p.
+//
+// The pruning procedure of the algorithm is described in the reference below.
+// Here, we provide parallel pruning implementation of this algorithm.
+//
+// Reference: Lam Si Tung Ho and Cécile Ané. A Linear-Time Algorithm for
+// Gaussian and Non-Gaussian Trait Evolution Models. SysBiol 2014.
+namespace ppa {
+template<class PruningTree>
+class ThreePointMultivariate: public PruningSpecification<PruningTree> {
+public:
+  typedef PruningSpecification<PruningTree> BaseType;
+  typedef PruningTree TreeType;
+  typedef vec NodeStateType;
+  typedef vec ParameterType;
+
+  // define fields as public in order to access them easily from R.
+  arma::mat X, Y;
+  arma::vec tTransf;
+  arma::mat hat_mu_Y, tilde_mu_X_prime;
+  arma::vec lnDetV, p, Q;
+
+  ThreePointMultivariate(Rcpp::List const& tree): BaseType(tree) {
+    this->tTransf = arma::vec(this->ref_tree_.num_nodes() - 1);
+    this->lnDetV = arma::vec(this->ref_tree_.num_nodes(), arma::fill::zeros);
+    this->p = arma::vec(this->ref_tree_.num_nodes(), arma::fill::zeros);
+    this->Q = arma::vec(this->ref_tree_.num_nodes(), arma::fill::zeros);
+  };
+
+  void set_X_and_Y(arma::mat const& X, arma::mat const& Y) {
+    if(X.n_rows != this->ref_tree_.num_tips() || Y.n_rows != this->ref_tree_.num_tips()) {
+      Rcpp::stop("The matrices X and Y must have the same number of rows as V.");
+    } else {
+      this->X = X; this->Y = Y;
+
+      this->hat_mu_Y = arma::mat(this->ref_tree_.num_nodes(), Y.n_cols, arma::fill::zeros);
+      this->tilde_mu_X_prime = arma::mat(this->ref_tree_.num_nodes(), X.n_cols, arma::fill::zeros);
+    }
+  }
+
+  NodeStateType StateAtRoot() const {
+    vec res(2);
+    res[0] = this->lnDetV(this->ref_tree_.num_nodes()-1);
+    res[1] = this->Q(this->ref_tree_.num_nodes()-1);
+    return res;
+  }
+
+  inline void initSpecialData() {
+    hat_mu_Y.fill(0);
+    tilde_mu_X_prime.fill(0);
+    lnDetV.fill(0);
+    p.fill(0);
+    Q.fill(0);
+  }
+
+  void InitNode(uint i) {
+    tTransf[i] = this->ref_tree_.LengthOfBranch(i);
+  }
+
+  void VisitNode(uint i) {
+    if(i < this->ref_tree_.num_nodes()) {
+      // branch leading to a tip
+      lnDetV[i] = log(tTransf[i]);
+      p[i] = 1 / tTransf[i];
+      hat_mu_Y.row(i) = Y.row(i);
+      tilde_mu_X_prime.row(i) = X.row(i);
+      Q[i] = arma::dot(X.row(i), Y.row(i)) / tTransf[i];
+    } else {
+      hat_mu_Y.row(i) /= p[i];
+      tilde_mu_X_prime(i) /= p[i];
+      Q[i] -= tTransf[i]*p[i]*p[i] / (1 + tTransf[i]*p[i]) *
+        arma::dot(tilde_mu_X_prime.row(i), hat_mu_Y.row(i));
+      lnDetV[i] += log(1 + tTransf[i]*p[i]);
+      p[i] /= (1 + tTransf[i]*p[i]);
+    }
+  }
+
+  void PruneNode(uint i, uint iParent) {
+    hat_mu_Y.row(iParent) += p[i]*hat_mu_Y.row(i);
+    tilde_mu_X_prime.row(iParent) += p[i]*tilde_mu_X_prime.row(i);
+    lnDetV[iParent] += lnDetV[i];
+    p[iParent] += p[i];
+    Q[iParent] += Q[i];
+  }
+};
+}
+#endif // ParallelPruning_ThreePointMultivariate_H_
